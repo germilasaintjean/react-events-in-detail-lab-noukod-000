9c7c476dccd1147f1ef2a62896e779db256886ec
Saint-Jean Germila <germila@noukod.com>
2019-04-23 17:29:53 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from "react";
+
+class CoordinatesButton extends Component {
+  onReceiveCoordinates (event) =  => {
+    cosole.log();
+    
+  }
+}
+
+render () {
+  return (
+    <div>
+    <button></button>
+    
+  )
+}
+
+export default CoordinatesButton
